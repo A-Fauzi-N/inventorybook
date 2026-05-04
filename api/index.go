3833e4922aacedd2eefa34f1a864685408786b21
A-Fauzi-N <korneolusv@gmail.com>
2026-05-04 14:20:16 +0700

    update yg ke4

--- api/index.go
+++ api/index.go
@@ -1,10 +1,9 @@
 package handler
 
 import (
-	"fmt"
+	"embed"
+	"html/template"
 	"net/http"
-	"os"
-	"path/filepath"
 	apps "inventorybook/app"
 	"inventorybook/auth"
 	"inventorybook/db"
@@ -13,38 +12,9 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-var (
-	app *gin.Engine
-)
+var templatesFS embed.FS
 
-func findTemplates() string {
-	wd, _ := os.Getwd()
-	fmt.Printf("[DEBUG] working dir: %s\n", wd)
-	
-	// Rekursif cari semua file
-	filepath.Walk("/var/task", func(path string, info os.FileInfo, err error) error {
-		if err == nil {
-			fmt.Printf("[DEBUG] file: %s\n", path)
-		}
-		return nil
-	})
-
-	candidates := []string{
-		"/var/task/templates/*",
-		"templates/*",
-		"../templates/*",
-		"/vercel/path0/templates/*",
-	}
-
-	for _, path := range candidates {
-		matches, _ := filepath.Glob(path)
-		if len(matches) > 0 {
-			return path
-		}
-	}
-
-	return "templates/*"
-}
+var app *gin.Engine
 
 func init() {
 	conn := db.InitDB()
@@ -53,9 +23,9 @@ func init() {
 	app = gin.New()
 	app.Use(gin.Logger(), gin.Recovery())
 
-	templatesPath := findTemplates()
-	fmt.Printf("[DEBUG] using templates path: %s\n", templatesPath)
-	app.LoadHTMLGlob(templatesPath)
+	// Load templates dari embed
+	tmpl := template.Must(template.ParseFS(templatesFS, "templates/*.html"))
+	app.SetHTMLTemplate(tmpl)
 
 	h := apps.New(conn)
 
